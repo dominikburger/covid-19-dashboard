ca16e7d5581b94f57f6fa4617a2d29d03ec365ff
Dominik Burger <dominik.burger@gmx.org>
2020-03-31 13:48:44 +0200

    Rename download scripts from make_* to download_*

--- src/data/make_geo_data.py
+++
@@ -1,6 +0,0 @@
-import os
-
-URL_GEO_DATA = 'https://www.naturalearthdata.com/\
-            http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip'
-
-os.system(f'curl {URL_GEO_DATA} -L -o ./data/external/geo_data/ne_50m_admin_0_countries.zip --create-dirs ')
