ee80478860f80b8b29f812a48b9c6c2466e068d4
Dominik Burger <dominik.burger@gmx.org>
2020-04-22 13:34:12 +0200

    Refactor dashboard layout to light theme

--- src/visualization/styles.py
+++ src/visualization/styles.py
@@ -1,7 +1,13 @@
+main_window_style = {
+    'backgroundColor': '#fafafa',
+    'width': 'device-width',
+}
+
 cumulated_info_style = {
     'border': '1px solid #d6d6d6',
     'border-radius': '0.25rem',
-    'backgroundColor': '#235174',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'display': 'flex',
     'flex-direction': 'column',
     'flex-shrink': 0,
@@ -13,34 +19,50 @@ cumulated_info_style = {
     'alignCenter': True
 }
 
+caption_case_style = {
+    'display': 'inline-block',
+    'color': '#363636',
+    'fontSize': 20,
+    'marginTop': 25,
+    'textAlign': 'center',
+    'width': '100%'
+}
+
+value_case_style = {
+    'display': 'inline-block',
+    'color': '#E14A4A',
+    'fontSize': 20,
+    'textAlign': 'center',
+    'width': '100%'
+}
+
 date_picker_style = {
-    'border': '1px solid #d6d6d6',
-    'backgroundColor': '#235174',
+    'backgroundColor': '',
     'display': 'flex',
     'marginTop': 10,
     'marginBottom': 5,
     'marginLeft': 10,
     'marginRight': 5,
-    'width': '25%'
+    'width': '15%'
 }
 
 main_graph_div_style = {
-    'border': '1px solid #d6d6d6',
-    'backgroundColor': '#235174',
+    'backgroundColor': '',
     'display': 'flex',
-    'marginTop': 15,
-    'marginBottom': 5,
-    'marginLeft': 10,
-    'marginRight': 5,
+    'marginTop': 0,
+    'marginBottom': 0,
+    'marginLeft': 0,
+    'marginRight': 0,
     'flex-direction': 'row',
     'width': '84%',
     'overflow': 'hidden'
 }
 
 map_graph_style = {
-    'backgroundColor': '#235174',
-    'display': 'flex',
-    'flex-shrink': 0,
+    'border': '1px solid #d6d6d6',
+    'border-radius': '0.25rem',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'marginTop': 15,
     'marginBottom': 5,
     'marginLeft': 5,
@@ -48,22 +70,24 @@ map_graph_style = {
     'width': '50%'
 }
 
-table_style = {
-    'backgroundColor': '#235174',
-    'display': 'flex',
-    'flex-shrink': 0,
+table_info_style = {
+    'border': '1px solid #d6d6d6',
+    'border-radius': '0.25rem',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'marginTop': 15,
     'marginBottom': 5,
     'marginLeft': 5,
     'marginRight': 5,
     'padding': 0,
-    'width': '49%'
+    'width': '50%'
 }
 
 toolbar_style = {
     'border': '1px solid #d6d6d6',
     'border-radius': '0.25rem',
-    'backgroundColor': '#235174',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'display': 'flex',
     'width': '15%',
     'flex-direction': 'column',
@@ -71,12 +95,11 @@ toolbar_style = {
     'marginTop': 5,
     'marginBottom': 5,
     'marginLeft': 5,
-    'marginRight': 15,
-    'padding': 0,
+    'marginRight': 10,
 }
 
 scale_style = {
-    'color': 'white',
+    'color': '#363636',
     'display': 'flex',
     'marginTop': 5,
     'marginBottom': 5,
@@ -97,28 +120,33 @@ country_picker_style = {
 info_graphs_style = {
     'display': 'flex',
     'width': '84%',
-    'overflow': 'hidden'
+    'backgroundColor': '',
+    'overflow': 'hidden',
+    'marginTop': 0,
+    'marginBottom': 0,
+    'marginLeft': 0,
+    'marginRight': 0,
 }
 
-timeseries_style = {
-    'border': '2px solid #d6d6d6',
-    'backgroundColor': 'white',
-    'display': 'flex',
-    'flex-shrink': 0,
+timeseries_div_style = {
+    'border': '1px solid #d6d6d6',
+    'border-radius': '0.25rem',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'marginTop': 5,
-    'marginBottom': 10,
+    'marginBottom': 5,
     'marginLeft': 5,
     'marginRight': 5,
     'width': '50%',
-    'height': '500px'
 }
 
 tabs_div_style = {
-    'border': '2px solid #d6d6d6',
-    'backgroundColor': 'white',
-    'display': 'flex',
+    'border': '1px solid #d6d6d6',
+    'border-radius': '0.25rem',
+    'box-shadow': '2px 2px 2px lightgrey',
+    'backgroundColor': '#f6f6f6',
     'marginTop': 5,
-    'marginBottom': 10,
+    'marginBottom': 5,
     'marginLeft': 5,
     'marginRight': 5,
     'width': '50%'
@@ -140,21 +168,4 @@ tab_selected_style = {
     'backgroundColor': '#119DFF',
     'color': 'white',
     'padding': '2px'
-}
-
-caption_case_style = {
-    'display': 'inline-block',
-    'color': 'white',
-    'fontSize': 20,
-    'marginTop': 25,
-    'textAlign': 'center',
-    'width': '100%'
-}
-
-value_case_style = {
-    'display': 'inline-block',
-    'color': 'red',
-    'fontSize': 20,
-    'textAlign': 'center',
-    'width': '100%'
 }
