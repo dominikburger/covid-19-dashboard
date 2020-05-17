e18da955a041bd0de139f89c56c0c295dd3a7bde
Dominik Burger <dominik.burger@gmx.org>
2020-05-17 16:55:24 +0200

    Move path definitions to src.paths

--- src/data/update_covid_data.py
+++ src/data/update_covid_data.py
@@ -3,10 +3,9 @@ from src import paths
 
 
 def main():
-    os.chdir(f'{paths.dir_csse_data}')
+    os.system(f'cd {paths.dir_csse_data}')
     os.system('git fetch && git pull')
 
 
 if __name__ == '__main__':
     main()
-
