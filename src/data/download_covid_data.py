75c27c74a17f95bf9fb0eb8af3b25d85f1cbec80
Dominik Burger <33334148+dominikburger@users.noreply.github.com>
2021-02-27 21:57:28 +0100

    Replace os.system with calls to subprocess.run for covid data and geo data

--- src/data/download_covid_data.py
+++ src/data/download_covid_data.py
@@ -1,24 +1,58 @@
-import os
-import argparse
 from src import paths
+import shutil
+import subprocess
 
-URL_COVID_DATA_GIT = 'https://github.com/CSSEGISandData/COVID-19'
-URL_COVID_DATA_SVN = 'https://github.com/CSSEGISandData/COVID-19' \
+
+URL_COVID_DATA_GIT: str = 'https://github.com/CSSEGISandData/COVID-19'
+URL_COVID_DATA_SVN: str = 'https://github.com/CSSEGISandData/COVID-19' \
                      '/trunk/csse_covid_19_data/csse_covid_19_daily_reports'
 
-DIR_COVID_DOWNLOAD = 'csse_covid_19_data/csse_covid_19_daily_reports'
+DIR_COVID_DOWNLOAD: str = 'csse_covid_19_data/csse_covid_19_daily_reports'
+
+
+def clean_directory():
+    """
+    Removes the directory where csse data is stored and creates it afterwards
+    """
+    shutil.rmtree(paths.dir_csse_data)
+    paths.dir_csse_data.mkdir(exist_ok=False)
+
+
+def do_sparse_checkout():
+    """
+
+    """
+    result_clone = subprocess.run(
+        [
+            "git",
+            "clone", f"{URL_COVID_DATA_GIT}",
+            "--no-checkout", paths.dir_csse_data,
+            "--depth", "1"
+        ],
+        cwd=paths.dir_csse_data,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE
+    )
+    result_set_sparse = subprocess.run(
+        [
+            "git", "sparse-checkout",
+            "set", f"{DIR_COVID_DOWNLOAD}"
+        ],
+        cwd=paths.dir_csse_data,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE
+    )
+    result_checkout = subprocess.run(
+        ["git", "checkout", "master"],
+        cwd=paths.dir_csse_data,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE
+    )
 
 
 def main():
-    os.system(
-        f'rm -r {paths.dir_csse_data} && mkdir {paths.dir_csse_data} &&'
-        f'cd {paths.dir_csse_data} &&'
-        f'git init &&'
-        f'git remote add -f origin {URL_COVID_DATA_GIT} &&'
-        f'git config core.sparseCheckout true &&'
-        f'echo "{DIR_COVID_DOWNLOAD}" >> .git/info/sparse-checkout &&'
-        f'git pull origin master'
-    )
+    clean_directory()
+    do_sparse_checkout()
 
 
 if __name__ == '__main__':
